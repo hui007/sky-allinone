1c9351df83c65b0789c3ae8d00f0f97a04b2589d
joshui <jianghui_job@163.com>
2018-04-26 18:01:05 +0800

    spring的高级自动装配知识点

--- src/main/java/com/sky/spring/advancedAutoWire/AutoWireConfiguration.java
+++ src/main/java/com/sky/spring/advancedAutoWire/AutoWireConfiguration.java
@@ -2,17 +2,67 @@ package com.sky.spring.advancedAutoWire;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.sky.mybatis.mybatisSpring.config.DruidSourceProperties;
+
 @Configuration
+@EnableConfigurationProperties(DruidSourceProperties.class)
+@PropertySource(value = "classpath:com/sky/spring/advancedAutoWire/MagicProperties.properties", 
+	encoding="utf-8")
 public class AutoWireConfiguration {
+	@Autowired
+	DruidSourceProperties druidSourceProperties;
+	
 	@Bean
+	@Profile("dev")
+	@Qualifier("embeddedDS")
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
 				.addScript("db/database-schema.sql")
 				.addScript("db/database-test-data.sql").build();
+	}
+	
+	@Bean
+	@Profile("product")
+	@Qualifier("productDS")
+	public DataSource dataSourceDruid() {
+		DruidDataSource datasource = new DruidDataSource();
+		datasource.setUrl(druidSourceProperties.getDbUrl());
+		datasource.setUsername(druidSourceProperties.getUsername());
+		datasource.setPassword(druidSourceProperties.getPassword());
+		datasource.setDriverClassName(druidSourceProperties.getDriverClassName());
+
+		return datasource;
+	}
+	
+	@Bean
+	@Conditional(MagicCondition.class)
+	@Primary
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public MagicBean magicBean() {
+		return new MagicBean();
+	}
+	
+	@Bean
+	@Conditional(MagicCondition.class)
+	@MagicQualifier
+	public MagicBean magicBeanQualifier() {
+		MagicBean magicBean = new MagicBean();
+		magicBean.setName("magicBeanQualifier");
+		return magicBean;
 	}
 }
---
+++ src/main/java/com/sky/spring/advancedAutoWire/MagicQualifier.java
@@ -0,0 +1,15 @@
+package com.sky.spring.advancedAutoWire;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface MagicQualifier {
+
+}
