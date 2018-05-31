53d2a9006ec36a0b40abd66a031d6ecab907d0c5
joshui <jianghui_job@163.com>
2018-05-31 11:41:01 +0800

    自定义错误提示页

--- src/test/java/com/sky/allinone/spring/SpringMvcTest.java
+++ src/test/java/com/sky/allinone/spring/SpringMvcTest.java
@@ -271,6 +271,20 @@ public class SpringMvcTest {
         assertThat(new File(destUrl).exists()).isTrue();
 	}
 	
+	/**
+	 * 自定义错误提示页，有三种方式。
+	 * 1、Spring Boot 将所有的错误默认映射到/error， 实现ErrorController，修改发生错误后的映射路径
+	 * 2、添加自定义的错误页面
+	 * html静态页面：在resources/public/error/ 下定义，如添加404页面： resources/public/error/404.html页面，中文注意页面编码；
+	 * 模板引擎页面：在templates/error/下定义，如添加5xx页面： templates/error/5xx.ftl
+	 * 注：templates/error/ 这个的优先级比较 resources/public/error/高
+	 * 3、使用注解@ControllerAdvice，这个在本项目已经有例子了
+	 */
+	@Test
+	public void errorPage() {
+		
+	}
+	
 	@Test
 	public void testPDFMvc() throws Exception {
 		mvc.perform(get("/file/downloadVoidPdf"));
