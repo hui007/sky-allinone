2f71b69400a2cfd3065928c8d63abb91905efc8d
joshui <jianghui_job@163.com>
2018-06-07 21:53:26 +0800

    email

--- src/test/java/com/sky/allinone/SuiteTest.java
+++ src/test/java/com/sky/allinone/SuiteTest.java
@@ -5,14 +5,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.sky.allinone.cache.CacheTest;
+import com.sky.allinone.email.EmailTest;
 import com.sky.allinone.mybatis.MybatisSuiteTest;
 import com.sky.allinone.redis.RedisTest;
 import com.sky.allinone.spring.SpringSecurityTest;
 import com.sky.allinone.spring.SpringSuiteTest;
+import com.sky.allinone.webSocket.WebSocketTest;
 
 @RunWith(Suite.class)  
 @Suite.SuiteClasses({MybatisSuiteTest.class,SpringSuiteTest.class, RedisTest.class, CacheTest.class,
-	SpringSecurityTest.class})  
+	SpringSecurityTest.class, WebSocketTest.class,EmailTest.class})  
 public class SuiteTest {
 
 }
---
+++ src/test/java/com/sky/allinone/webSocket/WebSocketTest.java
@@ -0,0 +1,13 @@
+package com.sky.allinone.webSocket;
+
+import org.junit.Test;
+
+public class WebSocketTest {
+	/**
+	 * 暂时没有写用例，直接使用resources下的html页面测试
+	 */
+	@Test
+	public void testTodo() {
+		
+	}
+}
