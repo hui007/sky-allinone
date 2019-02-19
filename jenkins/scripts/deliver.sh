1a41683d15ca85bb82539703a7f495d6945031ac
joshui <jianghui_job@163.com>
2019-02-19 11:03:07 +0800

    jenkins部署文件

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,3 +3,4 @@ rootPath="../../.."
 cd rootPath
 pwd
 scp ./target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
+#scp $ENV/target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
