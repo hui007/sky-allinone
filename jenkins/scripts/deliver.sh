204c50bed6a8887745dcb5e78c13958d172aca55
joshui <jianghui_job@163.com>
2019-02-19 18:28:54 +0800

    jenkins部署脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,5 +5,7 @@
 #cd /usr/bin
 pwd
 #ls
+#不清楚为啥会打包入@2目录
+cd "$(pwd|xargs -I {} echo {}@2)"
 scp ./target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
 #scp $ENV/target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
