167d803fd3569760ebd9f88bfd28401f21061ded
joshui <jianghui_job@163.com>
2019-02-20 16:00:18 +0800

    jenkins部署脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -7,5 +7,8 @@ pwd
 #ls
 echo '不清楚为啥会打包入@2目录'
 cd "$(pwd|xargs -I {} echo {}@2)"
-scp ./target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
+remoteHost=193.112.47.33
+remoteDir=/home/joshui
+scp ./target/allinone-0.0.1-SNAPSHOT.jar root@$remoteHost:$remoteDir
+ssh root@${remoteHost} "cd ${remoteDir}/; ./startAllinone.sh"
 #scp $ENV/target/allinone-0.0.1-SNAPSHOT.jar root@193.112.47.33:/home/joshui
